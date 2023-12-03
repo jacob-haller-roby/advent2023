f8d8b75bd11d6427c9ee1c4e31ff375b7dbe9907
Jacob Haller-Roby <jacobhaller-roby@Jacobs-MBP.home>
2023-12-03 16:35:51 -0600

    newline

--- src/utils/runTest.ts
+++ src/utils/runTest.ts
@@ -2,7 +2,7 @@ export default async (test: () => Promise<number> | number, label?: string) => {
   const start = performance.now();
   const result = await test();
   const end = performance.now();
-  console.log(`Test results${label ? ` for ${label}` : ''}:`)
+  console.log(`\nTest results${label ? ` for ${label}` : ''}:`)
   console.log(`The result is: ${result}`);
   console.log(`Runtime was: ${(end - start).toFixed(0)}ms`)
 }
