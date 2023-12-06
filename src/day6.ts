2f703f37a2ac3299b11aa5fb117adc32eeb6971d
Jacob Haller-Roby <jacobhaller-roby@Jacobs-MacBook-Pro.local>
2023-12-05 23:32:52 -0600

    better performance day6

--- src/day6.ts
+++ src/day6.ts
@@ -7,6 +7,10 @@ interface Race {
   time: number,
   distance: number
 }
+interface Range {
+  start: number,
+  end: number
+}
 
 const part1 = () => {
   return parseData(dataInput)
@@ -39,7 +43,8 @@ const getWinnerCount = (race: Race): number => {
 
 const part2 = (): number => {
   const race = parseBadKerningData(dataInput);
-  const highestLoser = findFirstWinner(race) - 1;
+  // const highestLoser = findFirstWinner(race) - 1;
+  const highestLoser = findFirstWinnerRecursive(race, {start: 0, end: race.time}) - 1;
   return race.time - (highestLoser * 2) - 1;
 }
 
@@ -61,6 +66,18 @@ const findFirstWinner = (race: Race): number => {
     const distance = i * (race.time - i)
     if (distance > race.distance) return i;
   }
+}
+
+const findFirstWinnerRecursive = (race: Race, remainingRange: Range) => {
+  if (remainingRange.start === remainingRange.end) {
+    return remainingRange.start;
+  }
+  const testValue = Math.floor((remainingRange.start + remainingRange.end) / 2);
+  const isWinner = testValue * (race.time - testValue) > race.distance;
+  if (isWinner) {
+    return findFirstWinnerRecursive(race, {start: remainingRange.start, end: testValue })
+  }
+  return findFirstWinnerRecursive(race, { start: testValue + 1, end: remainingRange.end })
 }
 
 await runMetricsTest(part1, 'Part 1');
